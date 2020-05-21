e5239048a5bb828366d9ef16286adb390915bf8c
nikolinagasic <nikolinagasic97@gmail.com>
2020-05-22 01:03:27 +0200

    Added https

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {LoginComponent} from './login/login.component';
 import {AdminHomePageComponent} from './admin-home-page/admin-home-page.component';
 import {AgentHomePageComponent} from './agent-home-page/agent-home-page.component';
 import {CustomerHomePageComponent} from './customer-home-page/customer-home-page.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'adminHomePage', component: AdminHomePageComponent},
   {path: 'agentHomePage', component: AgentHomePageComponent},
-  {path: 'customerHomePage', component: CustomerHomePageComponent}
+  {path: 'customerHomePage', component: CustomerHomePageComponent},
+  {path: 'messages', component: MessagesComponent}
 ];
 
 @NgModule({
